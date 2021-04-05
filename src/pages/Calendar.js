22a40bd796230e0aaeac2519f53496b30c15314f
Octetful <845204+octetful@users.noreply.github.com>
2021-04-05 19:43:30 +0100

    Add calendar view.

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,9 +1,16 @@
 import { withRouter } from "react-router";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 const Calendar = () => {
     return (
         <div className="App-header">
-            Calendar here
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                initialView="dayGridMonth"
+            />
         </div>
     );
 }
