4c4c0838ead5418485227a1f79c2637f5eac25e4
Octetful <845204+octetful@users.noreply.github.com>
2021-04-06 20:21:33 +0100

    Add a working tasks list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import BottomNavbar from './components/BottomNavbar';
 import {
@@ -11,21 +10,38 @@ import Home from './pages/Home';
 import Calendar from './pages/Calendar';
 import Tasks from './pages/Tasks';
 import TaskScheduler from './pages/TaskScheduler';
+import { MuiThemeProvider, createMuiTheme, makeStyles } from "@material-ui/core/styles";
+
 
 function App() {
   let history = useHistory();
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+        height: "100%",
+        backgroundColor: theme.palette.background.default,
+    },
+  }));
+  const classes = useStyles();
+  const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+    },
+  });
+
   return (
-    <div className="App">
-      <BrowserRouter history={history} >
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/calendar" component={Calendar} />
-          <Route path="/tasks" component={Tasks} />
-          <Route path="/scheduler" component={TaskScheduler} />
-        </Switch>
-        <BottomNavbar value={"home"} />
-      </BrowserRouter>
+    <div className="App" className={classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter history={history} >
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/calendar" component={Calendar} />
+            <Route path="/tasks" component={Tasks} />
+            <Route path="/scheduler" component={TaskScheduler} />
+          </Switch>
+          <BottomNavbar value={"home"} />
+        </BrowserRouter>
+      </MuiThemeProvider>
     </div>
   );
 }
--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,7 +1,70 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        padding: '10px',
+        backgroundColor: theme.palette.background.default,
+    },
+    NewTaskEntry: {
+        width: '75%',
+    }
+}));
+
+const createNewTaskEntry = (labelId) => {
+    return <ListItem key={1} 
+                role={undefined}
+                dense
+                button
+                >
+        <ListItemIcon>
+            <Checkbox edge="start"
+            checked={false}
+            disableRipple
+            inputProps= {{'aria-labelledby': labelId}}/>   
+        </ListItemIcon>
+        <ListItemText id={labelId} primary={`You got work to do!`} />
+    </ListItem>
+};
+
 export default function Tasks() {
+    const classes = useStyles();
+    const labelId = `checkbox-list-label-1`;
+    const [taskEntryText, setTaskEntryText] = useState('');
+
     return (
         <div className="App-header">
             And some tasks
+            <form className={classes.root}>
+                <TextField label="Enter a new task" 
+                        className={classes.NewTaskEntry}
+                        value={taskEntryText}
+                        onChange={(evt) => setTaskEntryText(evt.target.value) }
+                />
+                <Button color="primary" 
+                        variant="contained"
+                        className={classes.button}
+                        startIcon={<AddIcon />}
+                >
+                    Add
+                </Button>
+            </form>
+
+            <List className={classes.root}>
+                {
+                    createNewTaskEntry(labelId)
+                }
+            </List>
         </div>
-    );
-}
+        );
+    }
