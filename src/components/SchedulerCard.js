089271859276b1291976a6feb874789dc18dfabe
Octetful <845204+octetful@users.noreply.github.com>
2021-04-10 08:47:08 +0100

    Add editable fields to SchedulerCard.

--- src/components/SchedulerCard.js
+++ src/components/SchedulerCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { Button } from '@material-ui/core'
+import { Button, TextField } from '@material-ui/core'
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles({
     Card: {
@@ -14,17 +15,43 @@ const useStyles = makeStyles({
 
 export default function SchedulerCard(props) {
     const classes = useStyles();
+    const [title, setTitle] = useState(props.title);
+    const [description, setDescription] = useState(props.description);
+    const [priority, setPriority] = useState(props.priority || "low")
+
     return (
         <Card className={classes.Card}>
             <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                    Priority: {props.priority || "low"}!
+                <Typography color="textSecondary" gutterBottom> 
+                    <TextField
+                        select
+                        label="Priority"
+                        value={priority}
+                        onChange={(evt) => setPriority(evt.target.value)}
+                        inputProps={{ 'aria-label': 'naked' }}
+                        >
+                        <option key="high" value="high">
+                            High!
+                        </option>
+                        <option key="medium" value="medium">
+                            Medium
+                        </option>
+                        <option key="low" value="low">
+                            Low
+                        </option>
+                    </TextField>
                 </Typography>
-                <Typography variant="h5">
-                    {props.title}
+                <Typography variant="h3">
+                    <InputBase
+                        value={title}
+                        onChange={(evt) => setTitle(evt.target.value)}
+                        inputProps={{ 'aria-label': 'naked' }} />
                 </Typography>
                 <Typography variant="body2" component="p" gutterBottom>
-                    {props.description}
+                    <InputBase
+                        value={description}
+                        onChange={(evt) => setDescription(evt.target.value)}
+                        inputProps={{ 'aria-label': 'naked' }} />
                 </Typography>
                 <Typography color="textSecondary" variant="caption" display="block" gutterBottom>
                     Estimate = {props.estimate || "1"} hours
